4b45b841a3dcd5d6e6d857298f695c9123327c55
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-15 22:20:03 +0200

    Проверка логина при регистрации

---
+++ checkLogin.php
@@ -0,0 +1,18 @@
+<?php
+if(empty(trim($_POST['login']))) {
+	echo "The field is empty";
+	exit();
+}
+$connection = new Mongo();
+$con = new MongoClient();
+$connection = $con -> notes -> users;
+$cursor = $connection -> find();
+while($document = $cursor -> getNext())
+{
+	if($_POST['login'] == $document['login']) {
+		echo "Profile already exists";
+		exit();
+	}
+}
+echo "Profile is available";
+?>
