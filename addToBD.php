8e148e6da5bd639ed101891eb170409af26d07e6
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-16 11:20:36 +0200

    кодировка пароля

--- addToBD.php
+++ addToBD.php
@@ -4,10 +4,11 @@ $connection = new Mongo();
 $con = new MongoClient();
 
 $connection = $con -> notes -> users;
-$log = $_POST['personLogin'];
-$pass = $_POST['personPass'];
+$login = $_POST['personLogin'];
+$pass = sha1(md5($_POST['personPass']));
 $email = $_POST['personEmail'];
-$person = array("login" => $log, "email" => $email, "password" => $pass);
+$person = array("login" => $login, "email" => $email, "password" => $pass);
 $connection -> insert($person);
 $con -> close();
+header('Refresh: 0; URL=index.html');
 ?>
