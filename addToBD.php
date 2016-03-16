f927711bc002da8386ad321e1c21fe8decaa6d25
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-16 12:50:24 +0200

    установка cookie

--- addToBD.php
+++ addToBD.php
@@ -9,6 +9,21 @@ $pass = sha1(md5($_POST['personPass']));
 $email = $_POST['personEmail'];
 $person = array("login" => $login, "email" => $email, "password" => $pass);
 $connection -> insert($person);
+$user = $connection -> findOne(array("login" => $login));
+$hash = md5(genCode());
+setcookie('id', $user["_id"], time()+3600);
+setcookie('hash', $hash, time()+3600);
+$connection -> update(array('login' => $login), array('$set' => array('hash' => $hash)), array('upsert' => true));
 $con -> close();
-header('Refresh: 0; URL=index.html');
+header('Refresh: 1; URL=notesCollection.html');
+
+function genCode($length = 10) {
+	$chars = "zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP1234567890";
+	$code = "";
+	while (strlen($code) < $length) {
+		$char = $chars[mt_rand(0, strlen($chars)-1)];
+		$code .= $char;
+	}
+	return $code;
+}
 ?>
