32781c92a0a20c5490919e07d14fc667d259fab9
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-18 01:50:29 +0200

    поправка

--- addToBD.php
+++ addToBD.php
@@ -1,21 +1,19 @@
 <?php  
-$connection = new Mongo();
-
 $con = new MongoClient();
 
-$connection = $con -> notes -> users;
+$collection = $con -> notes -> users;
 $login = $_POST['personLogin'];
 $pass = sha1(md5($_POST['personPass']));
 $email = $_POST['personEmail'];
 $person = array("login" => $login, "email" => $email, "password" => $pass);
-$connection -> insert($person);
-$user = $connection -> findOne(array("login" => $login));
+$collection -> insert($person);
+$user = $collection -> findOne(array("login" => $login));
 $hash = md5(genCode());
 setcookie('id', $user["_id"], time()+3600);
 setcookie('hash', $hash, time()+3600);
-$connection -> update(array('login' => $login), array('$set' => array('hash' => $hash)), array('upsert' => true));
+$collection -> update(array('login' => $login), array('$set' => array('hash' => $hash)), array('upsert' => true));
 $con -> close();
-header('Refresh: 1; URL=notesCollection.html');
+header('Refresh: 0; URL=notesCollection.html');
 
 function genCode($length = 10) {
 	$chars = "zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP1234567890";
