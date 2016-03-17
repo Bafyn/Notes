267b8ce6ec46e92f02aa557e61ad0b5bbdacf5c8
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-17 22:28:09 +0200

    добавление E-mail

--- getData.php
+++ getData.php
@@ -1,13 +1,13 @@
 <?php 
 $fh = fopen("data.txt", 'w+') or die("Открыть файл не удалось");
-$connection = new Mongo();
 $con = new MongoClient();
-$connection = $con -> notes -> users;
-$cursor = $connection -> find();
+$collection = $con -> notes -> users;
+$cursor = $collection -> find();
+$con -> close();
 $counter = 0;
 while($document = $cursor -> getNext())
 {
-    fwrite($fh, ++$counter.".Login: ".$document["login"]." | "."Password: ".$document["password"]."\r\n") or die("Сбой записи файла");
+    fwrite($fh, ++$counter.".Login: ".$document["login"]." | "."E-mail: ".$document['email']." | "."Password: ".$document["password"]."\r\n") or die("Сбой записи файла");
 }
 fclose($fh);
 ?>
