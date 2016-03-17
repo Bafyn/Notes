2cd3e2f01ac045dfb95df6b04c9a8fd86c50e03e
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-18 01:52:36 +0200

    проверка cookie

---
+++ chAuth.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
+	$con = new MongoClient();
+	$collection = $con -> notes -> users;
+	$user = $collection -> findOne(array("_id" => new MongoId($_COOKIE['id'])));
+	if($user['hash'] === $_COOKIE['hash']) {
+		echo "ok";
+	}
+	else echo "Error";
+	$con -> close();
+}
+else {
+	echo "Error";
+}
+?>
