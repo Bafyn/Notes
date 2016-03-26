1f0baf8548dbbc76d6f49997afc311082670670b
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-26 21:53:39 +0200

    улучшен внешний вид(читаемость) кода

--- index.php
+++ index.php
@@ -12,36 +12,35 @@ if(isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Log in</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css" />
 	<link rel="icon" type="image/png" href="favicon.png" />
-	<script type="text/javascript" src = "script1.js"></script>
-	
+	<script type="text/javascript" src = "users_js.js"></script>
 </head>
+
 <body>
-<div id = "imgDiv">
-<img src="notes.png" id = "imgTitle">
-</div>
-<p id = "descr">Convenient means of storing notes!</p><br/>
-<div id = "noteAndFields">
-<div id="qwe">
-	<img src="note.png" id = "noteImg">
+	<div id = "imgDiv">
+		<img src="notes.png" id = "imgTitle" />
 	</div>
-
-	<div id = "fieldsDiv">
-	<form method="post"><br/>
-		<input type="text" name="personLogin" required id = "log" maxlength="20" placeholder = "Enter the login"/>
-		<p id = "errorLog" name = "errorLogin"></p>
-		<input type="password" name="personPass" required id = "pass" maxlength="25" placeholder = "Enter the password"/>
-		<p id = "errorPass" name = "errorPassword"></p>
-		<input type = "submit" name="submit" id = "btnLogin" onclick="CheckLoginAuthorization()" value="Log in"/><br/>
-		<a id = "createAccount" href="registration.php">Create account</a>
-	</form>
-
+	<p id = "descr">Convenient means of storing notes!</p><br/>
+	<div id = "noteAndFields">
+		<div id="mainNoteDiv">
+			<img src="note.png" id = "noteImg" />
+		</div>
+		<div id = "fieldsDiv">
+			<form method="post"><br/>
+				<input type="text" name="personLogin" required id = "log" maxlength="20" placeholder = "Enter the login or E-mail" />
+				<p id = "errorLog" name = "errorLogin"></p>
+				<input type="password" name="personPass" required id = "pass" maxlength="25" placeholder = "Enter the password" />
+				<p id = "errorPass" name = "errorPassword"></p>
+				<input type = "submit" name="submit" id = "btnLogin" onclick="CheckLoginAuthorization()" value="Log in" /><br/>
+				<a id = "createAccount" href="registration.php" />Create account</a>
+			</form>
+		</div>
 	</div>
-
-</div>
 	<footer id = "copy">Copyright&copy2016 Nikitin Dima</footer>
 </body>
+
 </html>
