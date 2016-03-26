73bc4d2fc9232e497e15a9f6ead58faa5beaf194
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-26 19:37:43 +0200

    улучшена запись данных из базы в файл(добавлена запись заметок)

--- getData.php
+++ getData.php
@@ -1,13 +1,24 @@
 <?php 
-$fh = fopen("data.txt", 'w+') or die("Открыть файл не удалось");
+$fh = fopen("users.txt", 'w+') or die("Открыть файл не удалось");
 $con = new MongoClient();
-$collection = $con -> notes -> users;
-$cursor = $collection -> find();
-$con -> close();
-$counter = 0;
-while($document = $cursor -> getNext())
+$collectionU = $con -> notes -> users;
+$cursorU = $collectionU -> find();
+$counter1 = 0;
+while($documentU = $cursorU -> getNext())
 {
-    fwrite($fh, ++$counter.".Login: ".$document["login"]." | "."E-mail: ".$document['email']." | "."Password: ".$document["password"]."\r\n") or die("Сбой записи файла");
+    fwrite($fh, ++$counter1.".Login: ".$documentU["login"]." | "."E-mail: ".$documentU['email']." | "."Password: ".$documentU["password"].";\r\n") or die("Сбой записи файла");
 }
 fclose($fh);
+
+$fh = fopen("memos.txt", 'w+') or die("Открыть файл не удалось");
+$collectionN = $con -> notes -> memos;
+$cursorN = $collectionN -> find();
+$counter2 = 0;
+while($documentN = $cursorN -> getNext())
+{
+    fwrite($fh, ++$counter2.".ID(user's): ".$documentN['id']." | "."title: ".$documentN['title']." | "."description: ".$documentN["description"]." | ".$documentN['date'].";\r\n") or die("Сбой записи файла");
+}
+fclose($fh);
+
+$con -> close();
 ?>
