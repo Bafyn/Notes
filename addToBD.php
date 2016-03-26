2239ea6e8aa0ac3ff2670d7fc78b700ce2e3bf6c
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-26 21:52:20 +0200

    улучшен внешний вид(читаемость) кода

--- addToBD.php
+++ addToBD.php
@@ -5,13 +5,18 @@ $collection = $con -> notes -> users;
 $login = $_POST['personLogin'];
 $pass = sha1(md5($_POST['personPass']));
 $email = $_POST['personEmail'];
+
 $person = array("login" => $login, "email" => $email, "password" => $pass);
 $collection -> insert($person);
+
 $user = $collection -> findOne(array("login" => $login));
 $hash = md5(genCode());
+
 setcookie('id', $user["_id"], time()+18000);
 setcookie('hash', $hash, time()+18000);
+
 $collection -> update(array('login' => $login), array('$set' => array('hash' => $hash)), array('upsert' => true));
+
 $con -> close();
 header('Refresh: 0; URL=notesCollection.html');
 
