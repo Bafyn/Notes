243e7812ce1bd9b26b50023f09d26474f13a2c76
Dima.Nikitin <nikitin2705@mail.ru>
2016-03-26 19:38:02 +0200

    добавлена иконка

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
 <head>
 	<title>Log in</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="icon" type="image/png" href="icon.png" />
+	<link rel="icon" type="image/png" href="favicon.png" />
 	<script type="text/javascript" src = "script1.js"></script>
 	
 </head>
@@ -23,7 +23,7 @@ if(isset($_COOKIE['id']) && isset($_COOKIE['hash'])) {
 <div id = "imgDiv">
 <img src="notes.png" id = "imgTitle">
 </div>
-<p id = "descr">Convenient means of storing notes!</p>
+<p id = "descr">Convenient means of storing notes!</p><br/>
 <div id = "noteAndFields">
 <div id="qwe">
 	<img src="note.png" id = "noteImg">
